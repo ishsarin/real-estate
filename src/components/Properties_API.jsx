e45f4e1c8752476a1f9a331b9be8a5739a2a53f1
ishsarin <ishsarin2000@gmail.com>
2024-03-11 21:26:47 +0530

    api changes

--- src/components/Properties_API.jsx
+++ src/components/Properties_API.jsx
@@ -5,23 +5,41 @@ const Properties_API = () => {
     getAPI();
   }, []);
 
+  const [d, getData] = useState([{}]);
+
   const getAPI = async () => {
     const res = await fetch(
-      "https://rentalsapi.zillowgroup.com/listings/v1/listingsForUser",
+      "https://bayut.p.rapidapi.com/properties/list?locationExternalIDs=5002%2C6020&purpose=for-rent&hitsPerPage=25&page=0&lang=en&sort=city-level-score&rentFrequency=monthly&categoryExternalID=3",
       {
         method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "e37de9475cmshc91754dee8d657cp1f9b19jsnf9309833a34a",
+          "X-RapidAPI-Host": "bayut.p.rapidapi.com",
+        },
       }
     );
 
     try {
-      const data = await res.text();
-      console.log(data);
+      const data = await res.json();
+      console.log(data.hits);
+      console.log(d);
+      getData(data.hits);
     } catch (e) {
       console.error(e);
     }
   };
 
-  return <div>Properties_API</div>;
+  return (
+    <>
+      <div>Properties_API</div>
+      <div>
+        {d.map((item, id) => (
+          <div key={id}>{item.agency.name}</div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Properties_API;
